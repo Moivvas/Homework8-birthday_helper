20263d67095b63bb38af24bfdbe9f540b1e7eae0
Moivvas <130219729+Moivvas@users.noreply.github.com>
2023-06-24 19:47:34 +0300

    remove print, output in str

--- get_birthdays_per_week.py
+++ get_birthdays_per_week.py
@@ -2,7 +2,7 @@ from datetime import datetime, timedelta
 from collections import defaultdict
 
 users = [{'name': 'Jill Valentine', 'birthday': '2000-6-27'},
-         {'name': 'Bill Overbeck', 'birthday': '1970-6-27'},
+         {'name': 'Bill Overbeck', 'birthday': '1970-6-29'},
          {'name': 'Feng Min', 'birthday': '2000-06-24'}
          ]
 
@@ -10,7 +10,6 @@ def next_monday_f():
     today = datetime.now().date()
     days_ahead = (0 - today.weekday()) % 7
     next_monday = today + timedelta(days=days_ahead)
-    print(next_monday)
     return next_monday
 
 def get_birthdays_per_week(lst):
@@ -22,9 +21,9 @@ def get_birthdays_per_week(lst):
         actual_user_birthday = datetime(year=current_datetime.year, month=user_birthday.month, day=user_birthday.day).date()
         if (next_monday - timedelta(days=2)) <= actual_user_birthday < (next_monday + timedelta(days=5)):
             if actual_user_birthday.weekday() in (5, 6):
-                party[next_monday].append(f'{user["name"]} - {actual_user_birthday}')
+                party[next_monday].append(f'{user["name"]}')
             else:
-                party[actual_user_birthday].append(f'{user["name"]} - {actual_user_birthday}')
+                party[actual_user_birthday].append(f'{user["name"]}')
         else:
             continue
     sorted_party = dict(sorted(party.items(), key=lambda x: x[0].weekday()))
@@ -32,4 +31,4 @@ def get_birthdays_per_week(lst):
 
 if __name__ == '__main__':
     for key, value in get_birthdays_per_week(users).items():
-        print(f"{key.strftime('%A')}: {value}")
+        print(f"{key.strftime('%A')}: {(', '.join(map(str, value)))}")
